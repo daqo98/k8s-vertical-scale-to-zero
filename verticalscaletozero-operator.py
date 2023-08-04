e7080c2d180501d8fe0921da6ccf738222b8b030
David Quintero <davidalfredo.quintero@mail.polimi.it>
2023-08-04 18:17:46 +0200

    Vertical scale for pod. Requests and limits can be adjusted

--- verticalscaletozero-operator.py
+++ verticalscaletozero-operator.py
@@ -14,18 +14,38 @@ except k8s_config.ConfigException:
 
 # create an instance of the API class
 api_instance = k8s_client.CoreV1Api()
-""" name = 'prime-numbers' # str | name of the Pod
-namespace = 'namespace_example' # str | object name and auth scope, such as for teams and projects
-body = NULL # object |  """
 pretty = 'pretty_example' # str | If 'true', then the output is pretty printed. (optional)
 
-try: 
-    #api_response = api_instance.patch_namespaced_pod(name, namespace, body, pretty=pretty)
-    #api_response = api_instance.read_namespaced_pod(name="prime-numbers", namespace="default", pretty=pretty)
-    api_response = k8s_client.AppsV1Api().read_namespaced_deployment(namespace="default", name="prime-numbers", pretty=pretty)
-    pprint(api_response)
-except k8s_client.rest.ApiException as e:
-    print("Exception when calling CoreV1Api->patch_namespaced_pod: %s\n" % e)
+
+def handlingException(apiCall):
+    try: 
+        #api_response = api_instance.patch_namespaced_pod(name, namespace, body, pretty=pretty)
+        podName = api_instance.list_namespaced_pod(namespace="default", pretty=pretty).items[0].metadata.name
+        api_response = api_instance.read_namespaced_pod(name=podName, namespace="default", pretty=pretty)
+        #api_response = k8s_client.AppsV1Api().read_namespaced_deployment(namespace="default", name="prime-numbers", pretty=pretty) # works
+        return(api_response)
+    except k8s_client.rest.ApiException as e:
+        print("Exception when calling CoreV1Api->patch_namespaced_pod: %s\n" % e)
+
+def update_resources_pod(namespace, name, new_data):
+    api_instance = k8s_client.CoreV1Api()
+    return api_instance.patch_namespaced_pod(name=name, namespace=namespace, body=new_data)
 
 
-#api_instance.read_namespaced_deployment(namespace="default",name=name)
+def modifyPodResources(pod, cpu_req, cpu_lim, mem_req, mem_lim):
+    pod.spec.containers[0].resources = {'claims': None,
+                                        'limits': {'cpu': '%sm' % cpu_lim,
+                                                   'memory': '%sMi' % mem_lim},
+                                        'requests': {'cpu': '%sm' % cpu_req,
+                                                     'memory': '%sMi' % mem_req}}
+    return pod
+
+def verticalScale(cpu_req, cpu_lim, mem_req, mem_lim):
+    namespace = "default"
+    podName = api_instance.list_namespaced_pod(namespace="default", pretty=pretty).items[0].metadata.name
+    api_response = api_instance.read_namespaced_pod_status(name=podName, namespace="default", pretty=pretty)
+    new_data = modifyPodResources(api_response, cpu_req, cpu_lim, mem_req, mem_lim)
+    pprint(new_data)
+    update_resources_pod(namespace,podName,new_data) 
+
+verticalScale(2, 2, 2, 2)
