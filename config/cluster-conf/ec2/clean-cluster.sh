ce6c3c610fed9324cae110987ab7e41e6cec61fc
David Quintero <davidalfredo.quintero@mail.polimi.it>
2023-09-07 00:38:11 +0200

    To handle cases when device or resource is busy

--- config/cluster-conf/ec2/clean-cluster.sh
+++ config/cluster-conf/ec2/clean-cluster.sh
@@ -11,7 +11,18 @@ if [[ $answer = y ]] ; then
   #sudo kubeadm reset --cri-socket=unix:///var/run/cri-dockerd.sock
 fi
 
+# If device or resource busy
+umount_times=$(mount | grep /var/lib/kubelet/ | wc -l)
+echo $umount_times
+if [[ $umount_times -ge 1 ]] ; then
+  for i in `seq 1 $umount_times`
+  do
+    sudo umount -lf tmpfs
+  done
+fi
+
 sudo rm -rf /etc/kubernetes /var/lib/kubelet /var/lib/etcd /etc/cni/net.d
+
 sudo kill -9 $(ps aux | grep kubelet | grep -v grep| awk '{print $2}')
 sudo iptables -F
 sudo iptables -t nat -F
