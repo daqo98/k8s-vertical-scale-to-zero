30c1ef013fc316f5f026159ebb2d4baa522b30e1
David Quintero <davidalfredo.quintero@mail.polimi.it>
2023-09-06 22:31:56 +0200

    Fixing cleaning script again

--- config/cluster-conf/ec2/ec2-clean.sh
+++ config/cluster-conf/ec2/ec2-clean.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-# Drain node
-kubectl drain $(hostname) --ignore-daemonsets --delete-emptydir-data --force
-
 read -p "Is this the master node? [y/n]" answer
 if [[ $answer = y ]] ; then
+  # Drain nodes
+  for nodename in $(kubectl get nodes | awk '{print $1}')
+  do
+    kubectl drain $(nodename) --ignore-daemonsets --delete-emptydir-data --force
+  done
+  # Reset cluster
   sudo kubeadm reset --cri-socket=unix:///var/run/containerd/containerd.sock
   #sudo kubeadm reset --cri-socket=unix:///var/run/cri-dockerd.sock
 fi
