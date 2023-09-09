632d52247a2c92fc9e597bb556bb3f43b7c1ae37
David Quintero <davidalfredo.quintero@mail.polimi.it>
2023-09-09 13:50:51 +0200

    Modifying rollback order

--- rollback.sh
+++ rollback.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Deleting old resources
-kubectl delete -f "config/crd/bases"
-kubectl delete -f "config/permissions"
-kubectl delete -f "config/storage"
-kubectl delete -f "application/deployment.yaml"
 kubectl delete -f "application/service.yaml"
+kubectl delete -f "application/deployment.yaml"
+kubectl delete -f "config/storage"
+kubectl delete -f "config/permissions"
+kubectl delete -f "config/crd/bases"
