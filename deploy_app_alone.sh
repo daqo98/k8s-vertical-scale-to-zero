e8260738193d3ae9fc6011d93cbddd70c9934f1a
Kylar Stern <pablo101991@gmail.com>
2023-09-10 15:44:31 +0200

    Create different configuration files

---
+++ deploy_app_alone.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install Custom Resource Definitions (CRDs)
+kubectl apply -f "config/crd/bases"
+
+# Install RBACs
+kubectl apply -f "config/permissions"
+
+# Install PV and PVC to store .csv of metrics-logger
+kubectl apply -f "config/storage"
+
+# Remove taint on the master or control-plane nodes to schedule pods on the Kubernetes control-plane node:
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all  node-role.kubernetes.io/control-plane-
+
+# Deploy app
+kubectl apply -f "app_alone/deployment.yaml"
+kubectl apply -f "app_alone/service.yaml"
+
+# kubectl port-forward service/prime-numbers 8080:80
+# kubectl port-forward service/http-metrics 8000:8000
+# Try a request: curl http://localhost:8080/prime/12 or curl http://<Node's_Public_IP>:31512/prime/12
+## Locust workload: 
+# cd $HOME/versca20
+# pipenv install
+# pipenv shell
+# locust -f ~/versca20/locust_workload.py --headless --host=http://localhost:8080/prime/12
